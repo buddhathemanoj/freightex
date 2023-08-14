ccf9e873d07bdb83714dee9c51ceb77372e9969b
Manoj Prabhakar <134469946+buddhathemanoj@users.noreply.github.com>
2023-08-14 09:01:09 +0000

    mm

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,176 +1,4 @@
-// // import React, { useState } from "react";
-// // import { getAuth } from 'firebase/auth';
-// // import { Col,Row, Form, Input, Button } from 'antd';
-// // import { createUserWithEmailAndPassword } from "firebase/auth";
-// // import auth from '../../firebase'
-// // export const Signup = () => {
-// //     const [form] = Form.useForm();
-// //     const [loading, setLoading] = useState(false);
-// //     const authInstance = getAuth();
-// //     const signUp =  (values) => {
-// //       setLoading(true);
-// //       const { email, password } = values;
-// //       createUserWithEmailAndPassword(authInstance, email, password)
-// //       .then((userCredential) => {
-// //         console.log(userCredential);
-// //       })
-// //       .catch((error) => {
-// //         console.log(error);
-// //       });
-// //     };
-  
-// //     return (
-// //       <div className="sign-in-container">
-// //         <Row justify="center">
-// //           <Col    xl={8} >
-// //         <Form form={form} onFinish={signUp}>
-// //           <h1>Create Account</h1>
-// //           <Form.Item
-// //             name="email"
-// //             rules={[{ required: true, type: 'email', message: 'Please enter your email' }]}
-// //           >
-// //             <Input placeholder="Enter your email" />
-// //           </Form.Item>
-// //           <Form.Item
-// //             name="password"
-// //             rules={[{ required: true, message: 'Please enter your password' }]}
-// //           >
-// //             <Input.Password placeholder="Enter your password" />
-// //           </Form.Item>
-// //           <Form.Item>
-// //             <Button type="primary" htmlType="submit" loading={loading}>
-// //               Sign Up
-// //             </Button>
-// //           </Form.Item>
-// //         </Form>
-// //         </Col>
-// //         </Row>
-// //        </div>
-// //     );
-// //   };
 
-// import React, { useState } from "react";
-// import { getAuth } from 'firebase/auth';
-// import { getFirestore } from 'firebase/firestore';
-// import { collection, addDoc, doc, setDoc } from 'firebase/firestore';
-// import { Col, Row, Form, Input, Button } from 'antd';
-// import { createUserWithEmailAndPassword } from "firebase/auth"
-// import { auth ,firestore } from '../../firebase'; // Make sure you have the correct path to firestore
-// import { Link } from "react-router-dom";
-
-// export const Signup = () => {
-//   const [form] = Form.useForm();
-//   const [loading, setLoading] = useState(false);
-//   const authInstance = getAuth();
- 
-//   const signUp =  async (values) => {
-//     setLoading(true);
-//     const { email, password, username } = values;
-//     try {
-//         const userCredential = await createUserWithEmailAndPassword(authInstance, email, password);
-
-//         // Store the username in Firestore
-//         const userDocRef = doc(firestore, 'users', userCredential.user.uid);
-//         await setDoc(userDocRef, { username });
-    
-
-//       console.log(userCredential);
-//     } catch (error) {
-//       console.log(error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   return (
-//     <div className="sign-in-container">
-//       <Row justify="center">
-//         <Col xl={8}>
-//           <Form form={form} onFinish={signUp}>
-//             <h1>Create Account</h1>
-//             <Form.Item
-//               name="email"
-//               rules={[{ required: true, type: 'email', message: 'Please enter your email' }]}
-//             >
-//               <Input placeholder="Enter your email" />
-//             </Form.Item>
-//             <Form.Item
-//               name="password"
-//               rules={[{ required: true, message: 'Please enter your password' }]}
-//             >
-//               <Input.Password placeholder="Enter your password" />
-//             </Form.Item>
-//             <Form.Item
-//               name="username"
-//               rules={[{ required: true, message: 'Please enter your username' }]}
-//             >
-//               <Input placeholder="Enter your username" />
-//             </Form.Item>
-//             <div style={{display:'flex' ,gap:'30px'}}>
-//                  <Form.Item>
-//               <Button type="primary" htmlType="submit" loading={loading}>
-//                 Sign Up
-//               </Button>
-//             </Form.Item>
-//             <Link to={'/login'}><p>Already a user?</p></Link>
-//             </div>
-           
-//           </Form>
-//         </Col>
-//       </Row>
-//     </div>
-//   );
-// };
-// import React, { useState } from "react";
-// import { getAuth } from 'firebase/auth';
-// import { Col,Row, Form, Input, Button } from 'antd';
-// import { createUserWithEmailAndPassword } from "firebase/auth";
-// import auth from '../../firebase'
-// export const Signup = () => {
-//     const [form] = Form.useForm();
-//     const [loading, setLoading] = useState(false);
-//     const authInstance = getAuth();
-//     const signUp =  (values) => {
-//       setLoading(true);
-//       const { email, password } = values;
-//       createUserWithEmailAndPassword(authInstance, email, password)
-//       .then((userCredential) => {
-//         console.log(userCredential);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//     };
-  
-//     return (
-//       <div className="sign-in-container">
-//         <Row justify="center">
-//           <Col    xl={8} >
-//         <Form form={form} onFinish={signUp}>
-//           <h1>Create Account</h1>
-//           <Form.Item
-//             name="email"
-//             rules={[{ required: true, type: 'email', message: 'Please enter your email' }]}
-//           >
-//             <Input placeholder="Enter your email" />
-//           </Form.Item>
-//           <Form.Item
-//             name="password"
-//             rules={[{ required: true, message: 'Please enter your password' }]}
-//           >
-//             <Input.Password placeholder="Enter your password" />
-//           </Form.Item>
-//           <Form.Item>
-//             <Button type="primary" htmlType="submit" loading={loading}>
-//               Sign Up
-//             </Button>
-//           </Form.Item>
-//         </Form>
-//         </Col>
-//         </Row>
-//        </div>
-//     );
-//   };
 
 import React, { useState } from "react";
 import { getAuth } from 'firebase/auth';
@@ -181,11 +9,15 @@ import { createUserWithEmailAndPassword ,sendEmailVerification} from "firebase/a
 import { auth ,firestore } from '../../firebase'; // Make sure you have the correct path to firestore
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+
+
 export const Signup = () => {
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
   const authInstance = getAuth();
  const navigate =useNavigate()
+
+
   const signUp =  async (values) => {
     setLoading(true);
     const { email, password, username } = values;
@@ -198,10 +30,15 @@ export const Signup = () => {
      
         await sendEmailVerification(userCredential.user);
         message.success("Sign-up successful! Please check your email for verification.");
-        setTimeout(() => {
-            navigate('/login');
-          }, 1000); //
-      console.log(userCredential);
+
+      setTimeout(() => {
+        // Check if the email is verified
+        if (userCredential.user.emailVerified) {
+          navigate('/login');
+        } else {
+          console.log("Email not verified yet.");
+        }
+      }, 1000);
     } catch (error) {
       console.log(error);
     } finally {
