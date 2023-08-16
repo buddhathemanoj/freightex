5c106caa84c18f10a9910f9312e88cfc53d92f42
Mohamad Meerasa <mohamadmeerasa45@gmail.com>
2023-08-16 17:18:15 +0530

    Route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import { useAuth } from "./firebase";
 import { Newshipment } from "./components/Ocean import/Newshipment";
 import HeaderComponent from "./components/Headers/Header";
 import Shipmentlist from "./components/Ocean import/Shipemntlist";
+import { Mainoverview } from "./components/Dashboard/Mainoverview";
+import MBLlist from "./components/Ocean import/MBLlist";
+import HBLlist from "./components/Ocean import/HBLlist";
+
 const { Content } = Layout;
 function App() {
   const currentUser = useAuth();
@@ -52,8 +56,31 @@ function App() {
       </Row>
     </Layout></>: <Navigate to="/login" />}/>
   
+    <Route path="/ocean-import/MB_L-list"  element={currentUser && currentUser.emailVerified ? <><HeaderComponent /> <Layout>
+      <Row>
+        <Col span={3}>
+          <Sidebarr />
+        </Col>
+       <Col span={21} >
+          <Content style={{paddingTop:'20px'}} >
+            < MBLlist/>
+          </Content>
+     </Col>
+      </Row>
+    </Layout></>: <Navigate to="/login" />}/>
 
-
+    <Route path="/ocean-import/HB_L-list"  element={currentUser && currentUser.emailVerified ? <><HeaderComponent /> <Layout>
+      <Row>
+        <Col span={3}>
+          <Sidebarr />
+        </Col>
+       <Col span={21} >
+          <Content style={{paddingTop:'20px'}} >
+            < HBLlist/>
+          </Content>
+     </Col>
+      </Row>
+    </Layout></>: <Navigate to="/login" />}/>
 
       <Route
         path="/gofreight"
@@ -64,8 +91,7 @@ function App() {
           </Col>
          <Col span={21} >
             <Content style={{paddingTop:'20px'}} >
-            
-              < Mainoverview/>
+              <Mainoverview/>
             </Content>
        </Col>
         </Row>
