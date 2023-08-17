75f9cb6835f8bb40b0c9bb6d8f0dacceb0f0f465
Kasi Viswanath <128577977+Vichu93@users.noreply.github.com>
2023-08-17 11:31:01 +0000

    Sidebar

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -40,116 +40,116 @@ const Sidebarrr = () => {
   const location = useLocation();
 
   return (
-    <Sider
-      collapsible
-      collapsed={collapsed}
-      onMouseEnter={() => setCollapsed(false)}
-      onMouseLeave={() => setCollapsed(true)}
-      style={{
-        backgroundColor: colorBgContainer1,
-        color: 'white',
-        minHeight: '100vh',
-      }}
-    >
-      <div className="demo-logo-vertical" />
-      <Menu
-        style={{ backgroundColor: colorBgContainer1, color: 'white' }}
-        mode="inline"
-        defaultSelectedKeys={[location.pathname]}
-        defaultOpenKeys={openSubMenuKeys}
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onMouseEnter={() => setCollapsed(false)}
+        onMouseLeave={() => setCollapsed(true)}
+        style={{
+          backgroundColor: colorBgContainer1,
+          color: 'white',
+          minHeight: '100vh',
+        }}
       >
-  <Menu.Item key="/gofreight" icon={<UserOutlined />}>
-    <Link to="/gofreight">Dashboard</Link>
-  </Menu.Item>
+        <div className="demo-logo-vertical" />
+        <Menu
+          style={{ backgroundColor: colorBgContainer1, color: 'white' }}
+          mode="inline"
+          defaultSelectedKeys={[location.pathname]}
+          defaultOpenKeys={openSubMenuKeys}
+        >
+          <Menu.Item key="/gofreight" icon={<UserOutlined />}>
+            <Link to="/gofreight">Dashboard</Link>
+          </Menu.Item>
 
-  <Menu.SubMenu
-  key="sub-ocean-import"
-  icon={<UserOutlined />}
-  title="Ocean Import"
-  onTitleClick={() => handleSubMenuClick('sub-ocean-import')}
-  open={openSubMenuKeys.includes('sub-ocean-import')}
->
-    <Menu.Item key="/ocean-import/new-shipment">
-      <Link to="/ocean-import/new-shipment">New Shipment</Link>
-    </Menu.Item>
-    <Menu.Item key="/ocean-import/shipment-list">
-      <Link to="/ocean-import/shipment-list">Shipment List</Link>
-    </Menu.Item>
-    <Menu.Item key="/ocean-import/shipment-list">
-      <Link to="/ocean-import/shipment-list">Master B/L List</Link>
-    </Menu.Item>
-    <Menu.Item key="/ocean-import/shipment-list">
-      <Link to="/ocean-import/shipment-list">House B/Layout List</Link>
-    </Menu.Item>
-  </Menu.SubMenu>
+          <Menu.SubMenu
+            key="sub-ocean-import"
+            icon={<UserOutlined />}
+            title="Ocean Import"
+            onTitleClick={() => handleSubMenuClick('sub-ocean-import')}
+            open={openSubMenuKeys.includes('sub-ocean-import')}
+          >
+            <Menu.Item key="/ocean-import/new-shipment">
+              <Link to="/ocean-import/new-shipment">New Shipment</Link>
+            </Menu.Item>
+            <Menu.Item key="/ocean-import/shipment-list">
+              <Link to="/ocean-import/shipment-list">Shipment List</Link>
+            </Menu.Item>
+            <Menu.Item key="/ocean-import/master-bl-list">
+              <Link to="/ocean-import/master-bl-list">Master B/L List</Link>
+            </Menu.Item>
+            <Menu.Item key="/ocean-import/house-bl-list">
+              <Link to="/ocean-import/house-bl-list">House B/L List</Link>
+            </Menu.Item>
+          </Menu.SubMenu>
 
-  <Menu.SubMenu
-    key="sub-ocean-export"
-    icon={<UserOutlined />}
-    title="Ocean Export"
-    onTitleClick={() => handleSubMenuClick('sub-ocean-export')}
-    open={openSubMenuKeys.includes('sub-ocean-export')}
-  >
-    <Menu.Item key="/ocean-export/new-shipment">
-      <Link to="/ocean-export/new-shipment">New Shipment</Link>
-    </Menu.Item>
-    <Menu.Item key="/ocean-export/shipment-list">
-      <Link to="/ocean-export/shipment-list">Shipment List</Link>
-    </Menu.Item>
-    <Menu.Item key="/ocean-export/shipment-list">
-      <Link to="/ocean-export/shipment-list">Master B/L List</Link>
-    </Menu.Item>
-    <Menu.Item key="/ocean-export/shipment-list">
-      <Link to="/ocean-export/shipment-list">House B/Layout List</Link>
-    </Menu.Item>
-    <Menu.Item key="/ocean-export/shipment-list">
-      <Link to="/ocean-export/shipment-list">New Booking</Link>
-    </Menu.Item>
-    <Menu.Item key="/ocean-export/shipment-list">
-      <Link to="/ocean-export/shipment-list">Booking List</Link>
-    </Menu.Item>
-    <Menu.Item key="/ocean-export/shipment-list">
-      <Link to="/ocean-export/shipment-list">New Vessel Schedule</Link>
-    </Menu.Item>
-    <Menu.Item key="/ocean-export/shipment-list">
-      <Link to="/ocean-export/shipment-list">Vessel Schedule List</Link>
-    </Menu.Item>
-  </Menu.SubMenu>
+          <Menu.SubMenu
+            key="sub-ocean-export"
+            icon={<UserOutlined />}
+            title="Ocean Export"
+            onTitleClick={() => handleSubMenuClick('sub-ocean-export')}
+            open={openSubMenuKeys.includes('sub-ocean-export')}
+          >
+            <Menu.Item key="/ocean-export/new-shipment">
+              <Link to="/ocean-export/new-shipment">New Shipment</Link>
+            </Menu.Item>
+            <Menu.Item key="/ocean-export/shipment-list">
+              <Link to="/ocean-export/shipment-list">Shipment List</Link>
+            </Menu.Item>
+            <Menu.Item key="/ocean-export/master-bl-list">
+              <Link to="/ocean-export/master-bl-list">Master B/L List</Link>
+            </Menu.Item>
+            <Menu.Item key="/ocean-export/house-bl-list">
+              <Link to="/ocean-export/house-bl-list">House B/Layout List</Link>
+            </Menu.Item>
+            <Menu.Item key="/ocean-export/new-booking">
+              <Link to="/ocean-export/new-booking">New Booking</Link>
+            </Menu.Item>
+            <Menu.Item key="/ocean-export/booking-list">
+              <Link to="/ocean-export/booking-list">Booking List</Link>
+            </Menu.Item>
+            <Menu.Item key="/ocean-export/new-vessel-schedule">
+              <Link to="/ocean-export/new-vessel-schedule">New Vessel Schedule</Link>
+            </Menu.Item>
+            <Menu.Item key="/ocean-export/vessel-schedule-list">
+              <Link to="/ocean-export/vessel-schedule-list">Vessel Schedule List</Link>
+            </Menu.Item>
+          </Menu.SubMenu>
 
-  <Menu.SubMenu
-    key="sub-sales"
-    icon={<UserOutlined />}
-    title="Sales"
-    onTitleClick={() => handleSubMenuClick('sub-sales')}
-    open={openSubMenuKeys.includes('sub-sales')}
-  >
-    <Menu.Item key="/sales/new-quotation">
-      <Link to="/sales/new-quotation">New Quotation</Link>
-    </Menu.Item>
-    <Menu.Item key="/sales/quotation-list">
-      <Link to="/sales/quotation-list">Quotation List</Link>
-    </Menu.Item>
-  </Menu.SubMenu>
+          <Menu.SubMenu
+            key="sub-sales"
+            icon={<UserOutlined />}
+            title="Sales"
+            onTitleClick={() => handleSubMenuClick('sub-sales')}
+            open={openSubMenuKeys.includes('sub-sales')}
+          >
+            <Menu.Item key="/sales/new-quotation">
+              <Link to="/sales/new-quotation">New Quotation</Link>
+            </Menu.Item>
+            <Menu.Item key="/sales/quotation-list">
+              <Link to="/sales/quotation-list">Quotation List</Link>
+            </Menu.Item>
+          </Menu.SubMenu>
 
-  <Menu.SubMenu
-    key="sub-trade-partner"
-    icon={<UserOutlined />}
-    title="Trade-partner"
-    onTitleClick={() => handleSubMenuClick('sub-trade-partner')}
-    open={openSubMenuKeys.includes('sub-trade-partner')}
-  >
-    <Menu.Item key="/trade-partner/new-quotation">
-      <Link to="/trade-partner/new-quotation">New Trade Partner</Link>
-    </Menu.Item>
-    <Menu.Item key="/trade-partner/quotation-list">
-      <Link to="/trade-partner/quotation-list">Trade Partner Credit Entry</Link>
-    </Menu.Item>
-    <Menu.Item key="/trade-partner/quotation-list">
-      <Link to="/trade-partner/quotation-list">Trade Partner List</Link>
-    </Menu.Item>
-  </Menu.SubMenu>
-</Menu>
-</Sider>
+          <Menu.SubMenu
+            key="sub-trade-partner"
+            icon={<UserOutlined />}
+            title="Trade-partner"
+            onTitleClick={() => handleSubMenuClick('sub-trade-partner')}
+            open={openSubMenuKeys.includes('sub-trade-partner')}
+          >
+            <Menu.Item key="/trade-partner/new-trade-partner">
+              <Link to="/trade-partner/new-trade-partner">New Trade Partner</Link>
+            </Menu.Item>
+            <Menu.Item key="/trade-partner/trade-partner-credit-entry">
+              <Link to="/trade-partner/trade-partner-credit-entry">Trade Partner Credit Entry</Link>
+            </Menu.Item>
+            <Menu.Item key="/trade-partner/trade-partner-list">
+              <Link to="/trade-partner/trade-partner-list">Trade Partner List</Link>
+            </Menu.Item>
+          </Menu.SubMenu>
+        </Menu>
+      </Sider>
   );
 };
 
